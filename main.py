ee9d0dfc1ffbf84e14dc79f8c4e320942008e5a0
AlekseevVladislav <50208526+AlekseevVladislav@users.noreply.github.com>
2023-06-18 14:17:27 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,25 +1,21 @@
-import json, os
+import json
+import pyaudio
+import requests
+import vosk
+import cv2
 
-import pyttsx3, pyaudio, vosk
+counter = 1
+model = vosk.Model("vosk-model-small-ru-0.4")
+response = requests.get("https://dog.ceo/api/breeds/image/random")
+data = json.loads(response.content)
 
-tts = pyttsx3.init('sapi5')
-
-voices = tts.getProperty('voices')
-tts.setProperty('voices', 'en')
-
-for voice in voices:
-    print(voice.name)
-    if voice.name == 'Microsoft Zira Desktop - English (United States)':
-        tts.setProperty('voice', voice.id)
-
-model = vosk.Model('model_small')
 record = vosk.KaldiRecognizer(model, 16000)
-pa = pyaudio.PyAudio()
-stream = pa.open(format=pyaudio.paInt16,
-                 channels=1,
-                 rate=16000,
-                 input=True,
-                 frames_per_buffer=8000)
+p = pyaudio.PyAudio()
+stream = p.open(format=pyaudio.paInt16,
+                channels=1,
+                rate=16000,
+                input=True,
+                frames_per_buffer=16000)
 stream.start_stream()
 
 
@@ -28,21 +24,63 @@ def listen():
         data = stream.read(4000, exception_on_overflow=False)
         if record.AcceptWaveform(data) and len(data) > 0:
             answer = json.loads(record.Result())
-            if answer['text']:
-                yield answer['text']
+            if answer["text"]:
+                yield answer["text"]
 
 
-def speak(say):
-    tts.say(say)
-    tts.runAndWait()
+def save_image(counter):
+    response = requests.get("https://dog.ceo/api/breeds/image/random")
+    image_url = response.json()["message"]
+    p = requests.get(image_url)
+    out = open("img"+str(counter)+".jpg", "wb")
+    out.write(p.content)
+    out.close()
+    print('Сохранено')
 
 
-speak('starting')
-print('start...')
+def dog_image():
+    response = requests.get("https://dog.ceo/api/breeds/image/random")
+    image_url = response.json()["message"]
+    print(image_url)
+    p = requests.get(image_url)
+    out = open("img.jpg", "wb")
+    out.write(p.content)
+    out.close()
+    print("Ваш собакен")
+    img = cv2.imread("img.jpg")
+    cv2.imshow("Dog", img)
+    cv2.waitKey(1)
+    cv2.destroyAllWindows()
+
+
+def dog_breed():
+    response = requests.get("https://dog.ceo/api/breeds/image/random")
+    data = response.json()
+    breeds = data["message"]
+    breeds = breeds.split("/")
+    print("Порода собаки:", breeds[-2])
+
+
+def resolution():
+    img = cv2.imread("img.jpg")
+    width, height, _ = img.shape
+    resolution = str(width) + " x " + str(height)
+    print(resolution)
+
+
 for text in listen():
-    if text == 'закрыть':
-        quit()
-    elif text == 'блокнот':
-        os.system('notepad.exe')
+    if "показать" in text:
+        dog_image()
+    elif "сохранить" in text:
+        save_image(counter)
+        counter+=1
+    elif "следующая" in text:
+        dog_image()
+    elif "порода" in text:
+        dog_breed()
+    elif "разрешения" in text:
+        resolution()
+    elif "выход" in text:
+        break
     else:
-        print(text)
+        print("Команда не распознана")
